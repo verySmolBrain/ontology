3c9f40d7f9c7d0c3f2d545d4d351712c13037b96
smolbrain <randomlyrandamu@gmail.com>
2023-05-22 01:48:28 +1000

    Got bpy to work

--- doc.md
+++ doc.md
@@ -1,1 +1,10 @@
-## Requirements
+## Installation Requirements
+
+This project relies on the Python module bpy which has the same requirements
+as Blender itself. We are also using bpy 3.5 which runs on Python3.10.
+To run on your own machine use this command:
+
+```
+pip install -r requirements.txt
+python generate_images/generate.py
+```
--- generate_images/generate.py
+++ generate_images/generate.py
@@ -1,5 +1,28 @@
+import bpy
+import addon_utils
+import os
+
+
 '''
 1. Generate images from .obj files
-2. Detect the object in each image
+2. Detect the object in each image (Exploded view)
 3. Classify each image as either a fastener or a non-fastener
 '''
+
+def main():
+    # print(bpy.path.abspath("//"))
+    cad_file = bpy.ops.wm.open_mainfile(filepath = "resources/gripper.blend")
+    print(cad_file)
+    
+    for collection in bpy.data.collections:
+        print(collection.name)
+        for obj in collection.all_objects:
+            print("obj: ", obj.name)
+    
+    print("AAAA")
+    print("BBBBB")
+
+if __name__ == "__main__":
+    print("------------ Ignore the above error message ------------")
+    
+    main()
